a71c7971b710366af2ca40bf771bcb06f2fc2a2a
Luca Herms <lucaherms@gmail.com>
2026-07-18 12:25:56 +0200

    Electron-Desktop-Projekt fuer Windows-EXE hinzufuegen

---
+++ desktop/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 520,
+    height: 900,
+    title: 'Sudoku',
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      sandbox: true,
+    },
+  });
+  win.loadFile(path.join(__dirname, '..', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
